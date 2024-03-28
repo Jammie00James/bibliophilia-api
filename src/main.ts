ff8f819da07c2660f5ba24d503c8030952b8e650
Jammie00James <omoeiyekewenjames@gmail.com>
2024-03-28 22:57:34 +0100

    get all trasc

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api/v1')
+  app.setGlobalPrefix('api')
   app.enableCors({
     origin: ['http://localhost:5173', 'http://localhost:5174', 'https://www.bibliophilia.com', 'https://bibliophilia.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
